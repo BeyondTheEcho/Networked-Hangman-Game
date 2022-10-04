2d123f5bde02daa87fcff17312919c040194501f
Stelios Klapsis <Steliosklapsis@outlook.com>
2022-10-03 22:20:13 -0400

    Additional File Commit For Dynamic IP/Port

--- Networked Hangman/Networked Hangman/NetworkManager.h
+++ Networked Hangman/Networked Hangman/NetworkManager.h
@@ -2,8 +2,11 @@
 #pragma comment (lib, "ws2_32.lib")
 
 #define WIN32_LEAN_AND_MEAN
+#include <string>
 #include <windows.h>
 #include <WinSock2.h>
+
+using namespace std;
 
 class NetworkManager
 {
@@ -22,7 +25,7 @@ public:
 	void Shutdown();
 	void CreateUDPSockets();
 	void BindUDP();
-	void SetRemoteData();
+	void SetRemoteData(int port, string cxIP);
 	void SendData(const char* data);
 	int ReceiveData(char* ReceiveBuffer);
 
